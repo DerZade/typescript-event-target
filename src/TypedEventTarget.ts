148d1287511964b985b8600576c6a66078de7905
DerZade <DerZade@gmail.com>
2024-04-22 22:04:46 +0200

    docs: :memo: add more jsdoc comments

--- src/TypedEventTarget.ts
+++ src/TypedEventTarget.ts
@@ -1,11 +1,33 @@
+/**
+ * A function that can be passed to the `listener` parameter of {@link TypedEventTarget.addEventListener} and {@link TypedEventTarget.removeEventListener}.
+ *
+ * @template M A map of event types to their respective event classes.
+ * @template T The type of event to listen for (has to be keyof `M`).
+ */
 export type TypedEventListener<M, T extends keyof M> = (
     evt: M[T]
 ) => void | Promise<void>;
 
+/**
+ * An object that can be passed to the `listener` parameter of {@link TypedEventTarget.addEventListener} and {@link TypedEventTarget.removeEventListener}.
+ *
+ * @template M A map of event types to their respective event classes.
+ * @template T The type of event to listen for (has to be keyof `M`).
+ */
 export interface TypedEventListenerObject<M, T extends keyof M> {
     handleEvent: (evt: M[T]) => void | Promise<void>;
 }
 
+/**
+ * Type of parameter `listener` in {@link TypedEventTarget.addEventListener} and {@link TypedEventTarget.removeEventListener}.
+ *
+ * The object that receives a notification (an object that implements the Event interface) when an event of the specified type occurs.
+ *
+ * Can be either an object with a handleEvent() method, or a JavaScript function.
+ *
+ * @template M A map of event types to their respective event classes.
+ * @template T The type of event to listen for (has to be keyof `M`).
+ */
 export type TypedEventListenerOrEventListenerObject<M, T extends keyof M> =
     | TypedEventListener<M, T>
     | TypedEventListenerObject<M, T>;
@@ -14,6 +36,25 @@ type ValueIsEvent<T> = {
     [key in keyof T]: Event;
 };
 
+/**
+ * Typescript friendly version of {@link EventTarget}
+ *
+ * @template M A map of event types to their respective event classes.
+ *
+ * @example
+ * ```typescript
+ * interface MyEventMap {
+ *     hello: Event;
+ *     time: CustomEvent<number>;
+ * }
+ *
+ * const eventTarget = new TypedEventTarget<MyEventMap>();
+ *
+ * eventTarget.addEventListener('time', (event) => {
+ *     // event is of type CustomEvent<number>
+ * });
+ * ```
+ */
 export interface TypedEventTarget<M extends ValueIsEvent<M>> {
     /** Appends an event listener for events whose type attribute value is type.
      * The callback argument sets the callback that will be invoked when the event
